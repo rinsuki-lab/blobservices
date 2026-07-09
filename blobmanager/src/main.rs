06f7b09470973f7dbcd4cfb52246b0858971ba5e
rinsuki <428rinsuki+git@gmail.com>
2026-07-09 23:20:13 +0900

    listen

--- blobmanager/src/main.rs
+++ blobmanager/src/main.rs
@@ -1,5 +1,18 @@
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
 #[tokio::main]
 async fn main() {
     blobservices_core::init_tracing_registry();
     tracing::info!("Hello, world!");
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, world!" }));
+
+    let listener = std::env::var("BLOBMANAGER_LISTEN_ADDR");
+    let listener = listener.unwrap_or_else(|_| { "0.0.0.0:3001".to_string() });
+    tracing::info!("trying to listen at {}", listener);
+    let listener = TcpListener::bind(listener)
+        .await.expect("failed to listen server");
+    axum::serve(listener, app).await.unwrap();
 }
