c373b17027682f6da1339a11d3cdaf20212fb95a
rinsuki <428rinsuki+git@gmail.com>
2026-07-16 00:30:13 +0900

    feat: put_blob_content_by_ref ちゃんとbodyを読むように

--- blobgateway/src/handlers/v1/put_blob_content_by_ref.rs
+++ blobgateway/src/handlers/v1/put_blob_content_by_ref.rs
@@ -1,12 +1,16 @@
+use std::sync::Arc;
+
 use axum::{
     body::Body,
     extract::{Path, State},
     response::{IntoResponse, Response},
 };
-use blobservices_core::proto::{self, storage::UploadBlobResponse};
+use blobservices_core::proto::{self};
+use futures::StreamExt;
 use http_body_util::BodyExt;
 use hyper::StatusCode;
 use prost::Message;
+use tokio::sync::Mutex;
 
 use crate::{NamespaceAndKey, state::AppState};
 
@@ -15,6 +19,7 @@ pub async fn put_blob_content_by_ref(
     Path(nk): Path<NamespaceAndKey>,
     body: Body,
 ) -> Result<(), Response> {
+    let body = Arc::new(Mutex::new(Some(body)));
     let mut store_configs = state
         .config
         .stores
@@ -26,7 +31,17 @@ pub async fn put_blob_content_by_ref(
     let mut upload_result = None;
 
     for (store_id, store_config) in store_configs {
-        if let Some(result) = upload_to_specific_store(&state, &nk, store_id, store_config).await {
+        {
+            let Some(_) = *body.lock().await else {
+                // 一回 100 Continue が帰ってきた後にエラーになってしまった場合、body はもう読まれた後なのでできることはない
+                // のでループを脱出する
+                // (バッファリングして再送とかはしない)
+                break;
+            };
+        }
+        if let Some(result) =
+            upload_to_specific_store(&state, &nk, store_id, store_config, &body).await
+        {
             upload_result = Some((store_id, result));
             break;
         }
@@ -84,19 +99,38 @@ async fn upload_to_specific_store(
     nk: &NamespaceAndKey,
     store_id: &str,
     store_config: &crate::config::StoreServerConfig,
+    body: &Arc<Mutex<Option<Body>>>,
 ) -> Option<proto::storage::UploadBlobResponse> {
     let mut req = store_config.url.clone();
     req.path_segments_mut().unwrap().push("v1").push("simple");
     req.query_pairs_mut()
         .append_pair("name_hint", &format!("{}/{}", nk.namespace, nk.key));
 
+    let (tx, rx) = tokio::sync::oneshot::channel();
+    let tx = Arc::new(Mutex::new(Some(tx)));
+
+    let body = body.clone();
+    // Expect: 100-continue を送り、帰ってくるまで body を消費したくないので、わざわざ reqwest ではなく hyper を使っている
+    // reqwest には 100 Continue を待つ機能がまだない https://github.com/seanmonstar/reqwest/issues/2845
     let req = hyper::Request::builder()
         .method(hyper::Method::POST)
         .version(hyper::Version::HTTP_11)
         .uri(req.as_str())
         .header(hyper::header::EXPECT, "100-continue")
         .header(hyper::header::ACCEPT, "application/protobuf")
-        .body(reqwest::Body::wrap("hello".to_string())); // TODO: 100 が来るまで待つ
+        .body(reqwest::Body::wrap_stream(
+            futures::stream::once(async move {
+                match rx.await {
+                    Ok(_) => {}
+                    Err(_) => return None,
+                };
+
+                let body = body.lock().await.take()?;
+                Some(body.into_data_stream())
+            })
+            .filter_map(async |x| x)
+            .flatten(),
+        ));
     let mut req = match req {
         Ok(req) => req,
         Err(e) => {
@@ -105,11 +139,23 @@ async fn upload_to_specific_store(
         }
     };
 
-    hyper::ext::on_informational(&mut req, move |res| {
-        println!("{:?}", res);
-    });
+    {
+        let tx = tx.clone();
+        hyper::ext::on_informational(&mut req, move |res| {
+            if res.status() != StatusCode::CONTINUE {
+                return;
+            }
+            let tx = tx.clone();
+            tokio::spawn(async move {
+                let tx = tx.lock().await.take();
+                let Some(tx) = tx else { return };
+                _ = tx.send(());
+            });
+        });
+    }
 
     let res = state.hyper_client.request(req).await;
+    drop(tx.lock().await.take()); // 必要あるのかわからないが、一応ここで rx を捨てておく (100 Continue 来ずに失敗した時用)
     let res = match res {
         Ok(r) => r,
         Err(e) => {
