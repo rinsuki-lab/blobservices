07c1617eca6aff94eb57c4c9c10ce3a851a31ed8
rinsuki <428rinsuki+git@gmail.com>
2026-07-16 11:54:16 +0900

    feat(gateway): head_blob_content_by_ref

--- blobgateway/src/handlers/v1/mod.rs
+++ blobgateway/src/handlers/v1/mod.rs
@@ -1,5 +1,7 @@
 mod get_blob_content_by_ref;
+mod head_blob_content_by_ref;
 mod put_blob_content_by_ref;
 
 pub use get_blob_content_by_ref::get_blob_content_by_ref;
+pub use head_blob_content_by_ref::head_blob_content_by_ref;
 pub use put_blob_content_by_ref::put_blob_content_by_ref;
