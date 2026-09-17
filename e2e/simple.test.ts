0f502a598c4aaeacf2a564c8ea88ed3c43548a57
rinsuki <428rinsuki+git@gmail.com>
2026-09-17 22:07:55 +0900

    chore: sliceのテストを追加 (まだ通らない)

--- e2e/simple.test.ts
+++ e2e/simple.test.ts
@@ -52,3 +52,42 @@ test("from other ref", async ({request}) => {
     })
     expect(await downloadRes.text()).toStrictEqual(data)
 })
+
+test("slice", async ({request}) => {
+    const data = "hello, world!"
+    const key1 = "slice-parent"
+    const key2 = "slice-child"
+
+    await request.put(`${URLS.blobgateway}/v1/content/by-ref/${namespace}/${key1}`, {
+        data,
+        failOnStatusCode: true,
+    })
+
+    await request.put(`${URLS.blobmanager}/v1/refs/${namespace}/${key2}`, {
+        data: toJson(PutBlobRefRequestSchema, create(PutBlobRefRequestSchema, {
+            content: {
+                case: "fromBlobSlice",
+                value: {
+                    source: {
+                        content: {
+                            case: "fromOtherRef",
+                            value: {
+                                namespace: namespace,
+                                key: key1,
+                            },
+                        },
+                    },
+                    start: 2n,
+                    size: 3n,
+                    hashes: {},
+                }
+            }
+        })),
+        failOnStatusCode: true,
+    })
+
+    const downloadRes = await request.get(`${URLS.blobgateway}/v1/content/by-ref/${namespace}/${key2}`, {
+        failOnStatusCode: true,
+    })
+    expect(await downloadRes.text()).toStrictEqual(data.slice(2, 5))
+})
